0d29a72da5d398885867dd01981ebba5807eb8dc
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-06-08 14:43:32 +0900

    docs: Register 라우트의 문서 추가

--- src/cmd/server/routes/routes.go
+++ src/cmd/server/routes/routes.go
@@ -2,11 +2,22 @@ package routes
 
 import (
 	"shoeguard-main-backend/cmd/server/controllers"
+	"shoeguard-main-backend/configs"
 
+	_ "shoeguard-main-backend/docs"
+
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 )
 
 func SetupRoutes(app *fiber.App) {
+	envs := configs.GetEnvs()
+	if envs.ENABLE_SWAGGER == "true" {
+		app.Get("/swagger/*", swagger.Handler)
+		app.Get("/swagger", func(c *fiber.Ctx) error {
+			return c.Redirect("./swagger/")
+		})
+	}
 	usersGroup := app.Group("/users")
 	usersGroup.Post("/register", controllers.Register)
 }
