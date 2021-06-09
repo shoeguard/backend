4dbb1e7ed1591f7187587d982888fa6d560338e4
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-06-09 15:47:05 +0900

    feat(controllers): GetMyInfo 추가

--- src/cmd/server/forms/user.go
+++ src/cmd/server/forms/user.go
@@ -7,3 +7,11 @@ type UserRegistrationForm struct {
 	PartnerPhoneNumber string `json:"partner_phone_number" example:"01012341234"   valid:"numeric, stringlength(11|11)"`
 	Nickname           string `json:"nickname"             example:"mengmota"      valid:"required, minstringlength(4)"`
 }
+
+type UserReadOnlyInfo struct {
+	PhoneNumber        string `json:"phone_number"         example:"01043214321" valid:"required, numeric, stringlength(11|11)"` // username field
+	IsStudent          bool   `json:"is_student"                                 valid:"required"`
+	PartnerPhoneNumber string `json:"partner_phone_number" example:"01012341234" valid:"numeric, stringlength(11|11)"`
+	Nickname           string `json:"nickname"             example:"mengmota"    valid:"required, minstringlength(4)"`
+}
+
