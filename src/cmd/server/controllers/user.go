f804fbd40e92ef630b870b9e038716845041d451
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-06-09 14:32:12 +0900

    refactor(controllers): 모두 같은 형식의 400 에러를 반환하도록 수정

--- src/cmd/server/controllers/user.go
+++ src/cmd/server/controllers/user.go
@@ -1,10 +1,11 @@
 package controllers
 
 import (
+	"shoeguard-main-backend/cmd/server/customErrors"
 	"shoeguard-main-backend/cmd/server/forms"
 	"shoeguard-main-backend/cmd/server/models"
+	"shoeguard-main-backend/cmd/server/utils"
 
-	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -15,20 +16,13 @@ import (
 // @Produce json
 // @Param body body forms.UserRegistrationForm true "body"
 // @Success 201 {object} forms.UserRegistrationForm
+// @Success 400 {object} customErrors.ErrorResponse
 // @Router /users/register [post]
 func Register(c *fiber.Ctx) error {
 	form := forms.UserRegistrationForm{}
 
-	// parsing body
-	if err := c.BodyParser(&form); err != nil {
-		return c.Status(400).
-			JSON(map[string]interface{}{"error": "form error", "detail": err.Error()})
-	}
-
-	// validating the body
-	if succeed, result := govalidator.ValidateStruct(form); !succeed {
-		return c.Status(400).
-			JSON(map[string]interface{}{"error": "form error", "detail": result.Error()})
+	if err := utils.ParseAndValidateForm(c, &form); err != nil {
+		return customErrors.Response400WithError(c, customErrors.FormError, err.Error())
 	}
 
 	user := models.User{
@@ -41,8 +35,11 @@ func Register(c *fiber.Ctx) error {
 
 	if err := user.Create(); err != nil {
 		if err.Error() == "phone number duplicates" {
-			return c.Status(400).
-				JSON(map[string]interface{}{"error": "phone number duplicates", "detail": "the user with the same phone number already exists."})
+			return customErrors.Response400WithError(
+				c,
+				customErrors.PhoneNumberDuplicate,
+				"The user with the same phone number already exists.",
+			)
 		}
 		return c.SendStatus(500)
 	}
