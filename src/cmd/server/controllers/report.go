bad68382b992aa2795de7d4588e2f68d34158294
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-06-09 10:37:46 +0900

    feat(controllers): AddRecordedAudioURL 추가

--- src/cmd/server/controllers/report.go
+++ src/cmd/server/controllers/report.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"shoeguard-main-backend/cmd/server/forms"
 	"shoeguard-main-backend/cmd/server/models"
+	"strconv"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
@@ -59,4 +60,47 @@ func Report(c *fiber.Ctx) error {
 	}
 
 	return c.Status(201).JSON(response)
+}
+
+// AddRecordedAudioURL godoc
+// @Summary Add recorded audio url
+// @Description Add recorded audio url to given ID's report
+// @security BasicAuth
+// @Accept json
+// @Produce json
+// @Success 200 {object} forms.AddRecordedAudioURLForm
+// @Router /report/{id} [patch]
+func AddRecordedAudioURL(c *fiber.Ctx) error {
+	form := forms.AddRecordedAudioURLForm{}
+	idStr := c.Params("id")
+	id, err := strconv.ParseUint(idStr, 10, 64)
+
+	if err != nil {
+		return c.Status(400).
+			JSON(map[string]interface{}{"error": "parameter error", "detail": "the paramter 'id' should be numeric"})
+	}
+
+	// parsing body
+	if err := c.BodyParser(&form); err != nil {
+		return c.Status(400).
+			JSON(map[string]interface{}{"error": "form error", "detail": err.Error()})
+	}
+
+	// validating body
+	if succeed, result := govalidator.ValidateStruct(form); !succeed {
+		return c.Status(400).
+			JSON(map[string]interface{}{"error": "form error", "detail": result.Error()})
+	}
+
+	report := models.Report{}
+	if err := report.SetReportByID(id); err != nil {
+		return c.SendStatus(404)
+	}
+
+	report.RecordedAudioURL = form.AudioURL
+	if err := report.Save(); err != nil {
+		return c.SendStatus(500)
+	}
+
+	return c.JSON(report)
 }
