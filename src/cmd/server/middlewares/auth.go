601583ac2273cccd729359c2b110bf75a6bcc993
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-06-09 15:49:33 +0900

    refactor(middlewares): BasicAuth 가 변수가 되도록 변경

--- src/cmd/server/middlewares/auth.go
+++ src/cmd/server/middlewares/auth.go
@@ -3,17 +3,13 @@ package middlewares
 import (
 	"shoeguard-main-backend/cmd/server/models"
 
-	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/basicauth"
 )
 
-func BasicAuthMiddleware() func(*fiber.Ctx) error {
-	basicAuth := basicauth.New(basicauth.Config{
-		Authorizer: func(phoneNumber string, password string) bool {
-			user := models.User{}
-			user.SetUser(phoneNumber)
-			return user.IsPasswordCorrect(password)
-		},
-	})
-	return basicAuth
-}
+var BasicAuth = basicauth.New(basicauth.Config{
+	Authorizer: func(phoneNumber string, password string) bool {
+		user := models.User{}
+		user.SetUser(phoneNumber)
+		return user.IsPasswordCorrect(password)
+	},
+})
