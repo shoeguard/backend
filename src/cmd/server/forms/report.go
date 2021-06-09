ac34e8694fe8bb38b7db484f076b501fe13480d8
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-06-09 09:14:02 +0900

    refactor(forms): ReportResponse 추가

--- src/cmd/server/forms/report.go
+++ src/cmd/server/forms/report.go
@@ -1,10 +1,16 @@
 package forms
 
+import "time"
+
 type ReportRequestForm struct {
 	DeviceInfo string  `example:"00:11:22:33:FF:EE"  valid:"required"`
 	Latitude   float64 `example:"37.5428147089301"   valid:"required"`
 	Longitude  float64 `example:"126.96631451849314" valid:"required"`
 }
+
+type ReportResponse struct {
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
 	DeviceInfo string  `example:"00:11:22:33:FF:EE"  valid:"required"`
 	Latitude   float64 `example:"37.5428147089301"   valid:"required"`
 	Longitude  float64 `example:"126.96631451849314" valid:"required"`
