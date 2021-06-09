97a43ce60fbbe2db60a55cdd6248e145daa20e84
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-06-09 09:13:26 +0900

    refactor(forms): ReportForm 이름 변경
    ReportForm -> ReportRequestForm

--- src/cmd/server/forms/report.go
+++ src/cmd/server/forms/report.go
@@ -1,6 +1,10 @@
 package forms
 
-type ReportForm struct {
+type ReportRequestForm struct {
+	DeviceInfo string  `example:"00:11:22:33:FF:EE"  valid:"required"`
+	Latitude   float64 `example:"37.5428147089301"   valid:"required"`
+	Longitude  float64 `example:"126.96631451849314" valid:"required"`
+}
 	DeviceInfo string  `example:"00:11:22:33:FF:EE"  valid:"required"`
 	Latitude   float64 `example:"37.5428147089301"   valid:"required"`
 	Longitude  float64 `example:"126.96631451849314" valid:"required"`
